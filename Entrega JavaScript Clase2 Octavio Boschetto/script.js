15cf0e5578f4044db616d56f75fde7d68ddab39f
Octavio <octavio.boschetto@gmail.com>
2021-05-08 10:37:20 -0300

    entregaClase2

---
+++ Entrega JavaScript Clase2 Octavio Boschetto/script.js
@@ -0,0 +1,12 @@
+var usuario = prompt("Ingrese su nombre de usuario:");
+var contrasenia= prompt("Ingrese su contraseña:");
+ 
+if ((!usuario )||(!contrasenia )){
+alert ("No ingresaste el nombre de usuario y/o contraseña")
+console.log ("No ingresaste el nombre de usuario y/o contraseña")
+}
+else {
+    ((usuario !=="") && (contrasenia !== ""));
+    alert ("Registro exitoso")
+    console.log ("Registro exitoso")
+};
